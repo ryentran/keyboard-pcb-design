5faaace311d3225f9d712070fc7bb8631c282a4b
Ryan <ryantran1031@gmail.com>
2019-08-20 23:39:13 -0700

    new projects

--- qmk_firmware/keyboards/3x3MacroPad/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/3x3MacroPad/keymaps/default/keymap.c
@@ -23,9 +23,9 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
-    KC_6, KC_7, KC_8,
-    KC_3, KC_4, KC_5,
-    KC_0, KC_1, KC_2 
+    KC_ESC, KC_A, KC_P,
+    KC_X  , KC_W, KC_K,
+    KC_F  , KC_M, KC_R 
   )
 };
 
